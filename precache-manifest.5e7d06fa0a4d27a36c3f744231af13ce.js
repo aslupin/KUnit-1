aeee136e41c9c2a6b4e2e76709bf0ecee2d711a1
poonsht <poon_arsene_lupin@hotmail.com>
2018-12-28 21:57:45 +0700

    Updates

---
+++ precache-manifest.5e7d06fa0a4d27a36c3f744231af13ce.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c807def43590896ba23c5850885db5ac",
+    "url": "/KUnit/static/media/tasklist.c807def4.svg"
+  },
+  {
+    "revision": "8ad138ebad66d864e414",
+    "url": "/KUnit/static/css/main.0f5bba2b.chunk.css"
+  },
+  {
+    "revision": "08926dd316c29c64fe518d1659d48029",
+    "url": "/KUnit/static/media/heart.08926dd3.svg"
+  },
+  {
+    "revision": "14e0a588888a5941eb53",
+    "url": "/KUnit/static/js/1.14e0a588.chunk.js"
+  },
+  {
+    "revision": "6739abc9df1229ec5cc4",
+    "url": "/KUnit/static/js/runtime~main.6739abc9.js"
+  },
+  {
+    "revision": "8ad138ebad66d864e414",
+    "url": "/KUnit/static/js/main.8ad138eb.chunk.js"
+  },
+  {
+    "revision": "1b3c5461f8b47580db521c115a09c9ad",
+    "url": "/KUnit/static/media/checklist.1b3c5461.svg"
+  },
+  {
+    "revision": "daa0112eebe4b8caa06e2bf36c7f6e42",
+    "url": "/KUnit/static/media/mortar-board.daa0112e.svg"
+  },
+  {
+    "revision": "207687a1a5ee00eb276adf2eebf83d5e",
+    "url": "/KUnit/static/media/mark-github.207687a1.svg"
+  },
+  {
+    "revision": "14e0a588888a5941eb53",
+    "url": "/KUnit/static/css/1.519476be.chunk.css"
+  },
+  {
+    "revision": "f2b60105a93457b846682a67fc9a3607",
+    "url": "/KUnit/index.html"
+  }
+];
