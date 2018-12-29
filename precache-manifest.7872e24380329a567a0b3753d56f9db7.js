ac93ef410f3b2ead3117cd18d4ec37782703de54
Wattanai Sathuphan <nine.stay27@gmail.com>
2018-12-29 16:58:46 +0700

    Updates

---
+++ precache-manifest.7872e24380329a567a0b3753d56f9db7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b3c5461f8b47580db521c115a09c9ad",
+    "url": "/KUnit/static/media/checklist.1b3c5461.svg"
+  },
+  {
+    "revision": "36549dddda0ac200e222",
+    "url": "/KUnit/static/css/main.ba9b55f7.chunk.css"
+  },
+  {
+    "revision": "8030e616ce030b3814cf5dc228e26149",
+    "url": "/KUnit/static/media/done.8030e616.svg"
+  },
+  {
+    "revision": "d485530d584f87537029",
+    "url": "/KUnit/static/js/1.d485530d.chunk.js"
+  },
+  {
+    "revision": "6739abc9df1229ec5cc4",
+    "url": "/KUnit/static/js/runtime~main.6739abc9.js"
+  },
+  {
+    "revision": "c807def43590896ba23c5850885db5ac",
+    "url": "/KUnit/static/media/tasklist.c807def4.svg"
+  },
+  {
+    "revision": "36549dddda0ac200e222",
+    "url": "/KUnit/static/js/main.36549ddd.chunk.js"
+  },
+  {
+    "revision": "daa0112eebe4b8caa06e2bf36c7f6e42",
+    "url": "/KUnit/static/media/mortar-board.daa0112e.svg"
+  },
+  {
+    "revision": "207687a1a5ee00eb276adf2eebf83d5e",
+    "url": "/KUnit/static/media/mark-github.207687a1.svg"
+  },
+  {
+    "revision": "08926dd316c29c64fe518d1659d48029",
+    "url": "/KUnit/static/media/heart.08926dd3.svg"
+  },
+  {
+    "revision": "91ca53900f8cec9de9daf4366886f3aa",
+    "url": "/KUnit/static/media/copy.91ca5390.svg"
+  },
+  {
+    "revision": "d485530d584f87537029",
+    "url": "/KUnit/static/css/1.b971bd04.chunk.css"
+  },
+  {
+    "revision": "08b9136cdb1c95e92524859293cf0d0f",
+    "url": "/KUnit/index.html"
+  }
+];
